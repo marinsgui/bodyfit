2d2746e1fdd49e9adeb6951396980d79e12be4ac
marinsgui <marins0guilherme@gmail.com>
2023-05-24 15:51:58 -0300

    seção sobre criada e lucide instalado

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Header />
       <HeroSection />
+      <About />
     </div>
   );
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,15 @@
+import { Users2, ArrowRightCircle } from 'lucide-react'
+
 export default function About() {
     return (
-        <section>
-            <h2>Nossa missão</h2>
-            
+        <section className="py-32 flex justify-center items-center">
+            <div className="flex flex-col gap-8 w-2/5 h-1/3 relative">
+                <Users2 size={100} color='#1313160D' className='absolute left-32 -top-8' />
+                <h2 className="text-6xl font-bold text-center z-10">Nossa missão</h2>
+                <p className="text-neutral-600 text-lg">Aqui, estamos comprometidos em ajudá-lo a alcançar seus objetivos e a transformar sua vida por meio do poder do fitness e do bem-estar.</p>
+                <p className="text-neutral-600 text-lg">Nossa missão é inspirar, motivar e capacitar cada indivíduo a se tornar a melhor versão de si mesmo. Estamos aqui para guiá-lo em sua jornada rumo a uma vida mais saudável e ativa.</p>
+                <button className='font-bold self-start flex gap-2 items-center'>FAÇA SUA MATRÍCULA <ArrowRightCircle /></button>
+            </div>
         </section>
     )
 }
